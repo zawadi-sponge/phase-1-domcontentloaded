10cd1ab26a288adb92287ce19891fc76b4a3e254
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-22 15:54:31 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes 
+document.addEventListener('DOMContentLoaded', () => { 
+    console.log('The DOM has loaded');
+
+
+ document.getElementById("text").textContent = "This is really cool!"
+});
